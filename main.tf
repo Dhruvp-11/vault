16f45751785b28c5ff36edd5fe1ba43ef35a6025
Dhruv Patel <78992905+Dhruvp-11@users.noreply.github.com>
2024-01-24 23:26:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "linux"{
     ami = "ami-00952f27cf14db9cd"
     instance_type = "t2.micro"
-    count = 2
+    count = 1
     key_name = "mykeys3"
     security_groups = ["ec2group"]
 
