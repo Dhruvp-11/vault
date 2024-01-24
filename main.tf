922548d2dfbcfe1a0363317ac5ea82c81f54b81c
Dhruv Patel <78992905+Dhruvp-11@users.noreply.github.com>
2024-01-24 14:58:17 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "linux"{
+    ami = "ami-00952f27cf14db9cd"
+    instance_type = "t2.micro"
+    count = 2
+    key_name = "mykeys3"
+    security_groups = ["ec2group"]
+
+    tags = {
+        Name = "vault-server" 
+    }
+}
+
+output "ip" {
+  value = aws_instance.linux.public_ip
+}
