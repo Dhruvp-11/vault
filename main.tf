f44be3c7df097f3dd46e3f98d86091d418e3a34a
Dhruv Patel <78992905+Dhruvp-11@users.noreply.github.com>
2024-01-27 11:21:53 +0530

    Update main.tf
    added comments

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "linux"{
-    ami = "ami-00952f27cf14db9cd"
-    instance_type = "t2.micro"
-    key_name = "mykeys3"
-    security_groups = ["ec2group"]
+    ami = "ami-00952f27cf14db9cd"  // ami -id of Amazon linux 
+    instance_type = "t2.micro"     // size
+    key_name = "mykeys3"           // key use to connect ec2 instance
+    security_groups = ["ec2group"]  // security group 8200,22,8201 open for vault
 
     tags = {
-        Name = "vault-server" 
+        Name = "vault-server"    // name of instance
     }
 }
 
 output "ip" {
-  value = aws_instance.linux.public_ip
+  value = aws_instance.linux.public_ip  // ipv4 public IP as a output
 }
